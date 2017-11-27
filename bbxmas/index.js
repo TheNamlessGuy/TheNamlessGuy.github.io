253a8ac93f1a6378891a667cc41f4a29e22acd35
Maximilian Bragazzi Ihrén <bragux@hotmail.com>
2017-11-27 21:29:26 +0100

    bbxmas mobile check

--- bbxmas/index.js
+++ bbxmas/index.js
@@ -1,8 +1,30 @@
-window.onload = function() {
-	open = document.getElementsByClassName("open");
-	for (var elem in open) {
-		elem.onclick = function() {
-			alert("This is open");
-		}
-	}
+var img = document.getElementsByClassName("calendar-img")[0];
+var boxes = document.getElementsByClassName("boxes")[0]
+
+function create_box(i) {
+	var w = ((img.width - 0.1 * img.width) / 6) * 0.95;
+	var h = ((img.height - 0.1 * img.height) / 10) * 0.95;
+	var x = (0.04 * img.width + ((i - 1) % 5) * (1.1 * w));
+	var y = -(0.96 * img.height - Math.floor((i - 1) / 5) * (1.1 * h));
+
+	var node = document.createElement("li");
+	boxes.appendChild(node);
+	node.classList.add("day" + (i + 1));
+	
+	node.style.width = w + "px";
+	node.style.height = h + "px";
+	node.style.top = y + "px";
+	node.style.left = x + "px";
+	
+	var innerNode = document.createElement("a");
+	node.appendChild(innerNode);
+	innerNode.innerHTML = i;
+	innerNode.href = "";
 }
+
+(function() {
+	for (var i = 1; i < 26; i++) {
+		console.log("Creating box " + i);
+		create_box(i);
+	}
+})();

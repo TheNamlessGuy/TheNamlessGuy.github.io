4890ff307f2363ba92d41dd90c2d2184348e1e8e
TheNamlessGuy <bragux@hotmail.com>
2022-03-01 13:44:52 +0100

    music

--- youtube_player/playlists/music.js
+++ youtube_player/playlists/music.js
@@ -642,4 +642,6 @@ PLAYLISTS.add('music', [
   {id: 'iX_TFkut1PM', title: 'Don McLean - American Pie'},
   {id: '4B6E0mijV5A', title: 'Wax Fang - Majestic'},
   {id: '2SUwOgmvzK4', title: 'Tame Impala - The Less I Know the Better'},
+  {id: 'qpHvj5pvvhg', title: 'SiM - The Rumbling'},
+  {id: '6a6dctE-ZLU', title: 'Delta Spirit - California'},
 ]);

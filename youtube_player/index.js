31afd31d59a6a7df4d5dbe3a7ea59eb214290f59
TheNamlessGuy <bragux@hotmail.com>
2022-03-23 19:30:58 +0100

    :/

--- youtube_player/index.js
+++ youtube_player/index.js
@@ -383,8 +383,8 @@ const YOUTUBE = {
 
 function helperEventHandler(e) {
   console.log('HELPER EVENT GET', e);
-  if (e.action === 'setup-playlist') {
-    PLAYLISTS.loadData(e.ids, true);
+  if (e.detail.action === 'setup-playlist') {
+    PLAYLISTS.loadData(e.detail.ids, true);
   }
 }
 

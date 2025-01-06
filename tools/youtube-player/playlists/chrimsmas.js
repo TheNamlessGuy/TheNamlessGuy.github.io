0ab9b606956a8ec1b4d98da975098a4bd57d44c2
TheNamlessGuy <thenamlessguy+github@hotmail.com>
2025-01-06 19:42:22 +0100

    music

---
+++ tools/youtube-player/playlists/chrimsmas.js
@@ -0,0 +1,11 @@
+Playlist.addPredesignedPlaylist('chrimsmas', [
+  {source: 'youtube', src: 'Bo3dc0fvXDg', title: 'We Need a Jingle Cat!'},
+  {source: 'youtube', src: 'wiELchl3n9E', title: 'Pleasure Elf'},
+  {source: 'youtube', src: 'p9ZSZFwVqqc', title: 'Carrot For A Cock'},
+  {source: 'youtube', src: 'qOVLUiha1B8', title: 'Welcome To YogLabs'},
+  {source: 'youtube', src: 'OMv_EPMED8Y', title: 'Brand New Friend'},
+  {source: 'youtube', src: 'lS-BfZfBbvU', title: 'Minecraft Christmas'},
+  {source: 'youtube', src: 'DP2vmsftZHY', title: 'Does Santa Claus...?'},
+  {source: 'youtube', src: 'XX1pZUwUi_U', title: 'Grussy'},
+  {source: 'youtube', src: 'j9jbdgZidu8', title: 'The Pogues - Fairytale of New York'},
+])
